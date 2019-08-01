06e6fb720f7111b41328dbc4dc3218bb3f805f5a
Jacob Porter <jastuper@gmail.com>
2019-08-01 16:52:11 -0400

    Updated config.py for Rivanna.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 """Stores the paths to tools."""
 
-SAMTOOLS = "/scratch/fungcat/jsporter/Applications/samtools-1.9/"
-BEDTOOLS = "/scratch/fungcat/jsporter/Applications/bedtools2/bin/"
+SAMTOOLS = "/home/jsp4cu/Applications/samtools-1.9/"
+BEDTOOLS = "/home/jsp4cu/Applications/bedtools2/bin/"
