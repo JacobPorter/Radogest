6620df718bfd683d92e1317565a1386fd53e279c
Jacob Porter <jastuper@gmail.com>
2019-11-28 12:51:22 -0500

    Modified to check the distribution by rank.

--- utils/check_distribution.py
+++ utils/check_distribution.py
@@ -9,14 +9,20 @@ Radogest generated fasta file.
 """
 import argparse
 import datetime
+import pickle
 import sys
-from SeqIterator import SeqIterator
 from collections import defaultdict
 
+from ete3 import NCBITaxa
 
-def check_distribution(fasta_file):
+from SeqIterator import SeqIterator
+
+ncbi = NCBITaxa()
+
+
+def check_distribution(fasta_file, index_file, rank):
     """
-    Get the distribution of the taxids in teh fasta file.
+    Get the distribution of the taxids in the fasta file.
 
     Parameters
     ----------
@@ -29,12 +35,22 @@ def check_distribution(fasta_file):
         A dictionary giving the counds of taxids.
 
     """
+    index = pickle.load(open(index_file, "rb"))
     seq_reader = SeqIterator(fasta_file, file_type="fasta")
-    sk_dict = defaultdict(int)
+    rank_dict = defaultdict(int)
+    total = 0
     for record in seq_reader:
-        sk = int(record[0].split(":")[1])
-        sk_dict[sk] += 1
-    return sk_dict
+        genome_acc = record[0].split(":")[0]
+        total += 1
+        try:
+            taxid = int(index["genomes"][genome_acc]['species_taxid'])
+            ranks = ncbi.get_rank(ncbi.get_lineage(taxid))
+        except (ValueError, KeyError) as e:
+            continue
+        for tid in ranks:
+            if ranks[tid] == rank:
+                rank_dict[tid] += 1
+    return rank_dict
 
 
 def main():
@@ -43,11 +59,22 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=__doc__)
-    parser.add_argument("fasta_file", type=str,
+    parser.add_argument("fasta_file",
+                        type=str,
                         help=("The Radogest fasta file to check."))
+    parser.add_argument("--index",
+                        "-i",
+                        type=str,
+                        help=("The location of the index."),
+                        default="./index_initial.pck")
+    parser.add_argument("--rank",
+                        "-r",
+                        type=str,
+                        help=("The rank to calculate a distribution for."),
+                        default="superkingdom")
     args = parser.parse_args()
     print(args, file=sys.stderr)
-    counts = check_distribution(args.fasta_file)
+    counts = check_distribution(args.fasta_file, args.index, args.rank)
     print(counts, file=sys.stdout)
     toc = datetime.datetime.now()
     print("The process took time: {}.".format(toc - tic), file=sys.stderr)
